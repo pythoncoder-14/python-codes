6736470c5a3a3f6bd72275fde665ba284cde0da6
pythoncoder-14 <jenselyne.raj@gmail.com>
2024-07-13 12:08:30 +0530

    how to do multiplication in python

---
+++ multiplication.py
@@ -0,0 +1,6 @@
+a=int(input("from where should it start: "))
+b=int(input("untill where should it multiply: "))
+c=int(input("which tables should it be: "))
+while a<=b:
+    print(a,"x",c,"=",a*c)
+    a+=1
